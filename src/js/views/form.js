0ef27ed55e8648d7d9c63a8fae697a9430c2f5d0
odettelittle <Odettelittle1@gmail.com>
2022-04-14 00:55:41 +0000

    primerooocommit

---
+++ src/js/views/form.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+export const Form = () => {
+	const [data, setData] =
+		useState({
+			full_name: "",
+			email: "",
+			phone:"",
+			address:"",
+			agenda_slug:"my_library",
+			email:"",
+		});
+  
+    return (
+
+	useEffect(() => {
+
+
+		fetch("https://assets.breatheco.de/apis/fake/contact/agenda/my_library")
+  		.then(response => response.text())
+  		.then(result => console.log(result))
+ 	 	.catch(error => console.log('error', error));
+	}, []);
+
+
+/////////////////////////
+
+const addContact = (newContact) => {
+      const newData = [...data,{ ]
+
+)        
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -13,3 +13,4 @@ export const Home = () => (
 		</a>
 	</div>
 );
+
