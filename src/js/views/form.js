c5f23f820e9e95bb59b897985fc985802e7ad03b
odettelittle <Odettelittle1@gmail.com>
2022-04-14 01:01:22 +0000

    segundocommit

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -20,7 +20,7 @@ export const Form = () => {
   		.then(response => response.text())
   		.then(result => console.log(result))
  	 	.catch(error => console.log('error', error));
-	}, []);
+	}, [])
 
 
 /////////////////////////
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,6 +2,10 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+import {form} from "./component/form.js";
+
+
+
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
